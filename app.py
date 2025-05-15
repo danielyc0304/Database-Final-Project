8ff5e4cace2e1413b916a3a259fd2748c8227f3e
YouCheng Lin <danielyc.mail0304@gmail.com>
2025-05-15 17:24:52 +0800

    feat(about): Add the structure of the about page

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -6,6 +6,11 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return "<p>Hello World</p>"
+
+
+@app.route("/about/<user_id>")
+def about(user_id):
+    return render_template("about.html")
 
 
 if __name__ == "__main__":
